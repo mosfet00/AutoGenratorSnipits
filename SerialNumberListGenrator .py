741f8d2e6efab4aa4b2a476cd5c7ccdf73ea4d0f
mosfet00 <asaleemi@epteck.com>
2025-06-30 00:56:21 -0700

    Update SerialNumberListGenrator

--- SerialNumberListGenrator .py
+++ SerialNumberListGenrator .py
@@ -1,9 +1,10 @@
-#Remotwell vfd                   RWVF03240100
+#Rewellx vfd                     RWVF03240100
 #Em sknit and sfill and s pump   SFST04240100
 #skem old                        SKEM03240100
 #em96                            EM9601240100
 #smartkinit CE                   SKMM07240100
-#Remotwell MAX v2                RWMX02240200
+#Rewellx MAX v2                  RWMX02240200
+#Rewellx MAX v2.0 (Black)        RWMX02250200
 
 
 # Define the base string and the range
@@ -20,3 +21,27 @@ for i in range(start, end + 1):
     else:
         # Format with '-' for numbers above the threshold
         print(f"[-{base_string}{i:002d}N-]")
+
+
+
+
+# GSM LIST  - [+GSMtested-100+]
+
+# tools https://phrasefix.com/tools/merge-text/
+
+
+# # Define the base string and the range
+# base_string = "GSMtested-"  # Base string to be used in the sequence
+# start = 1  # Starting number for the sequence
+# end = 100  # Ending number for the sequence
+# threshold = 100  # Threshold to switch from '+' to '-'
+
+# # Loop through the range and format the strings
+# for i in range(start, end + 1):
+#     if i <= threshold:
+#         # Format with '+' for numbers up to the threshold
+#         print(f" - [+{base_string}{i:002d}N+]")
+#     else:
+#         # Format with '-' for numbers above the threshold
+#         print(f"[-{base_string}{i:002d}N-]")
+        
